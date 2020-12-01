13a2d239152d4b4c6044f64d8b38e94907d5662f
이준희 <joonnc@yonsei.ac.kr>
2020-12-01 15:58:20 +0900

    Initial commit.

---
+++ src/main/java/com/dev/joon/SpringReactScratchApplication.java
@@ -0,0 +1,13 @@
+package com.dev.joon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactScratchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactScratchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/joon/SpringReactScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.joon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactScratchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
